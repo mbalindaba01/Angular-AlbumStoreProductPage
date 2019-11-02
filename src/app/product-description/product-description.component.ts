45e6a99cc262a4db0ad83b6957ffcaad78e07814
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2019-11-02 13:44:01 +0200

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,6 +8,8 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
